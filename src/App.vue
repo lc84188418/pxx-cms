886a1bee5e97a45c5dd5a2d835337ad443ebaa43
liuchun <1120397412@qq.com>
2021-12-02 01:06:16 +0800

    划分目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -15,12 +11,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/css/normalize.css';
 </style>
